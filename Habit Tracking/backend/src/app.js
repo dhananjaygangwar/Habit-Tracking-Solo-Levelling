9be8e8dcd8954e891ff875c4768e25b9e73f4bf2
Dhananjay Gangwar <kannu.2097@gmail.com>
2026-01-25 12:31:29 +0530

    auth

--- Habit Tracking/backend/src/app.js
+++ Habit Tracking/backend/src/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+// Routes
+const authRoutes = require("./routes/authRoutes");
+//for test db
+const pool = require("./config/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.json({ status: "Backend running" });
 });
+
+//for test db conn
+// app.get("/db-test", async (req, res) => {
+//   const result = await pool.query("SELECT NOW()");
+//   console.log(result);
+//   res.json(result.rows[0]);
+// });
 
 module.exports = app;
---
+++ Habit Tracking/backend/src/controllers/authController.js
@@ -0,0 +1,84 @@
+const pool = require("../config/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const userExists = await pool.query(
+      "SELECT id FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (userExists.rows.length > 0) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await pool.query(
+      `INSERT INTO users (username, email, password_hash)
+       VALUES ($1, $2, $3)
+       RETURNING id, username, email, level, xp`,
+      [username, email, hashedPassword]
+    );
+
+    res.status(201).json(newUser.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const userResult = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (userResult.rows.length === 0) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const user = userResult.rows[0];
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      token,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        level: user.level,
+        xp: user.xp
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { register, login };
